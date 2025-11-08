d88df39e95f155df139e375d71f9fc4f804bcbd7
Lee Mills <lee@leemills.dev>
2025-11-08 12:50:03 +0000

    chore: remove lando

--- assets/composer/settings.php
+++ assets/composer/settings.php
@@ -754,9 +754,6 @@ $settings['entity_update_backup'] = TRUE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-elseif (getenv('LANDO_SERVICE_NAME') == 'appserver' && file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
-  include $app_root . '/' . $site_path . '/settings.lando.php';
-}
 
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
